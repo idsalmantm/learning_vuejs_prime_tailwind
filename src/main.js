e45207d3fb9a3b3cabdb32d6e363214e66d610f7
Salman Hameed <idsalmantm@hotmail.com>
2024-12-19 01:06:10 +0400

    working

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from "primevue/config";
+import { definePreset } from '@primevue/themes';
 import Aura from "@primevue/themes/aura";
+import ToastService from 'primevue/toastservice';
+
 
 
 
@@ -12,10 +15,29 @@ import router from './router'
 
 const app = createApp(App)
 
+const FindhubPreset = definePreset(Aura, {
+  semantic: {
+      primary: {
+          50: '{green.50}',
+          100: '{green.100}',
+          200: '{green.200}',
+          300: '{green.300}',
+          400: '{green.400}',
+          500: '{green.500}',
+          600: '{green.600}',
+          700: '{green.700}',
+          800: '{green.800}',
+          900: '{green.900}',
+          950: '{green.950}'
+      }
+  }
+});
+
+
 app.use(createPinia())
 app.use(PrimeVue, {
   theme: {
-    preset: Aura,
+    preset: FindhubPreset,
     options: {
       prefix: "p",
       darkModeSelector: "system",
@@ -24,7 +46,9 @@ app.use(PrimeVue, {
     ripple: true,
     inputVariant: "outlined",
   },
+  
 });
-app.use(router)
 
+app.use(router)
+app.use(ToastService)
 app.mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,63 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="card flex justify-center">
+    <InputText type="text" v-model="value" />
+    <Button label="Verify" />
+  </div>
+  <div>
+    <ToggleSwitch v-model="checked1" />
+    <ToggleSwitch v-model="checked2" :dt="amberSwitch" />
+  </div>
+  <div>
+    <h1>Home</h1>
+    <div class="card">
+        <Toast />
+        <FileUpload name="demo[]" url="/api/upload" @upload="onAdvancedUpload($event)" :multiple="true" accept="image/*" :maxFileSize="1000000">
+            <template #empty>
+                <span>Drag and drop files to here to upload.</span>
+            </template>
+        </FileUpload>
+    </div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+<script setup>
+import { ref } from 'vue';
+import { useToast } from 'primevue/usetoast';
+
+const toast = useToast();
+
+const onAdvancedUpload = () => {
+    toast.add({ severity: 'info', summary: 'Success', detail: 'File Uploaded', life: 3000 });
+};
+const checked1 = ref(true);
+const checked2 = ref(true);
+const amberSwitch = ref({
+  handle: {
+    borderRadius: '3px'
+  },
+  colorScheme: {
+    light: {
+      root: {
+        checkedBackground: '{amber.500}',
+        checkedHoverBackground: '{amber.600}',
+        borderRadius: '4px'
+      },
+      handle: {
+        checkedBackground: '{amber.50}',
+        checkedHoverBackground: '{amber.100}'
+      }
+    },
+    dark: {
+      root: {
+        checkedBackground: '{amber.400}',
+        checkedHoverBackground: '{amber.300}',
+        borderRadius: '4px'
+      },
+      handle: {
+        checkedBackground: '{amber.900}',
+        checkedHoverBackground: '{amber.800}'
+      }
+    }
   }
-}
-</style>
+});
+</script>
